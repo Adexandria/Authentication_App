1a524e0179eeb2e2240e2c0ee69846427403360c
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2023-08-13 13:44:15 +0100

    Add DTOs

---
+++ SimpleAuthenticationAPI/Models/DTOs/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleAuthenticationAPI.Models.DTOs
+{
+    public class UserDTO
+    {
+        public UserDTO(string firstName, string lastName, string accessToken)
+        {
+            Name = $"{firstName} {lastName}";
+            AccessToken = accessToken;
+        }
+        public string Name { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
